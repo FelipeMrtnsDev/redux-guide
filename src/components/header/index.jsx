65a23b382d0d5de0b9199d9748111a940a94ae6e
felipe martins <juninpernadecano@gmail.com>
2024-07-30 16:17:04 -0400

    add and remove products ready

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import { useSelector, useDispatch } from 'react-redux'
 
 // Components
@@ -11,10 +11,14 @@ import { loginUser, logoutUser } from "../../redux/user/actions";
 function Header() {
   const [cartIsVisible, setCartIsVisible] = useState(false);
 
-  const { currentUser} = useSelector((rootReducer) => rootReducer.userReducer)
+  const { currentUser } = useSelector((rootReducer) => rootReducer.userReducer)
+  const { products } = useSelector((rootReducer) => rootReducer.cartReducer)
+
   const dispatch = useDispatch()
 
-  console.log({currentUser})
+  const productsCount = useMemo(() => {
+    return products.reduce((acc, curr) => acc + curr.quantity, 0)
+  }, [products])
 
   const handleCartClick = () => {
     setCartIsVisible(true);
@@ -37,8 +41,7 @@ function Header() {
         ) : (
           <div onClick={handleLoginClick}>Login</div>
         )}
-        <div onClick={handleLoginClick}>Login</div>
-        <div onClick={handleCartClick}>Carrinho</div>
+        <div onClick={handleCartClick}>Carrinho ({productsCount})</div>
       </Styles.Buttons>
 
       <Cart isVisible={cartIsVisible} setIsVisible={setCartIsVisible} />
